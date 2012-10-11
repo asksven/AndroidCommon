61f35df120c59e08703fd1ccbc9bb02891cc0d2e
asksven <sven.knispel@gmail.com>
2012-10-11 21:03:41 +0200

    Added leading/trailing quote trimming

--- src/com/asksven/android/common/utils/StringUtils.java
+++ src/com/asksven/android/common/utils/StringUtils.java
@@ -103,6 +103,18 @@ public class StringUtils
 		return value != null ? Long.parseLong(value) : 0;
 	}
 
+	public static String stripLeadingAndTrailingQuotes(String str)
+	  {
+	      if (str.startsWith("\""))
+	      {
+	          str = str.substring(1, str.length());
+	      }
+	      if (str.endsWith("\""))
+	      {
+	          str = str.substring(0, str.length() - 1);
+	      }
+	      return str;
+	  }
 
 
 }
