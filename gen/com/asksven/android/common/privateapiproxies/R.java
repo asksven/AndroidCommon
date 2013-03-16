58eea4e8819557caa63a62135022a630c5c9f3bb
asksven <sven.knispel@gmail.com>
2013-03-16 23:21:02 +0100

    nvm

--- gen/com/asksven/android/common/privateapiproxies/R.java
+++ gen/com/asksven/android/common/privateapiproxies/R.java
@@ -13,11 +13,28 @@ public final class R {
     public static final class drawable {
         public static int icon=0x7f020000;
     }
+    public static final class id {
+        public static int buttonClose=0x7f050001;
+        public static int buttonMarket=0x7f050003;
+        public static int buttonTwitter=0x7f050002;
+        public static int webview=0x7f050000;
+    }
     public static final class layout {
         public static int main=0x7f030000;
+        public static int readmewebview=0x7f030001;
     }
     public static final class string {
         public static int app_name=0x7f040001;
+        public static int app_pname=0x7f040002;
+        public static int dialog_apps=0x7f040009;
+        public static int dialog_dismiss=0x7f04000a;
+        public static int dialog_follow_me=0x7f04000b;
         public static int hello=0x7f040000;
+        public static int label_button_no=0x7f040005;
+        public static int label_button_rate=0x7f040004;
+        public static int label_button_remind=0x7f040003;
+        public static int market_link=0x7f040008;
+        public static int text_dialog_rate=0x7f040006;
+        public static int twitter_link=0x7f040007;
     }
 }
